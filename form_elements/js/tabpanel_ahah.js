07d634b5504a76dfefa9ef5f0c552976a922cd2f
Nigel Banks <nigel.g.banks@gmail.com>
2011-03-22 12:11:47 -0300

    Can now set tabpanels to be collapsible or collapsed by default. Requires theme's to be rebuilt.

--- form_elements/js/tabpanel_ahah.js
+++ form_elements/js/tabpanel_ahah.js
@@ -1,15 +1,36 @@
-$(document).ready(function() { 
-    var tabs = $(".action-expand-tab").tabs({collapsible: true, selected: -1 });
-    $(".ui-icon-circle-plus").live("click", function() { 
-        var id = $(this).text();
-        $("#"+id).trigger("mousedown");
-    });
-    $(".ui-icon-close").live("click", function() { 
-        var id = $(this).text();
-        $("#"+id).trigger("mousedown");
-    });
+formbuilder = {
+    tabpanel: {
+        loadPanels: function () {
+            var load = '.action-load-tabpanel';
+            var collapsible = '.property-collapsible-tabpanel';
+            var collapsed = '.property-collapsed-tabpanel';
+            var allTabs = $(load);
+            var collapsibleTabs = allTabs.filter(collapsible);
+            var nonCollapsibleTabs = allTabs.not(collapsible);
+            var expandedTabs = collapsibleTabs.not(collapsed);
+            var collapsedTabs = collapsibleTabs.filter(collapsed);
+            collapsedTabs.tabs( {collapsible: true, selected: -1 });
+            expandedTabs.tabs( {collapsible: true });
+            nonCollapsibleTabs.tabs({});
+        },
+        enableActions: function () {
+            $(".ui-icon-circle-plus").live("click", function() {
+                var id = $(this).text();
+                $("#"+id).trigger("mousedown");
+            });
+            $(".ui-icon-close").live("click", function() {
+                var id = $(this).text();
+                $("#"+id).trigger("mousedown");
+            });
+        }
+    } 
+};
+
+$(document).ready(function() {
+    formbuilder.tabpanel.loadPanels();
+    formbuilder.tabpanel.enableActions();
     $("body").ajaxComplete(function(event, request, settings) {
-        var tabs = $(".action-expand-tab").tabs({collapsible: true}); 
+        formbuilder.tabpanel.loadPanels();
         var response = eval("(" + request.responseText + ")");
         jQuery.extend(Drupal.settings, response.settings);
         Drupal.attachBehaviors();
